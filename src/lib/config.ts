3d92c7996a520f10126c9b30343ebadb92b06847
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2020-06-12 07:59:24 +0700

    edit: env

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,13 +1,14 @@
 import './env';
 
 export default class Config {
-    static ENABLE = true;
-    static VERSION = 'v2.1.6';
-    static PREFIX = '.';
-    static OWNER = '306616861456465924';
+    static ENV = process.env.APP_ENV;
+    static MONGODB = process.env.APP_MONGODB_URI;
+    static TOKEN = process.env.APP_DISCORD_TOKEN;
+    static TOKEN_APIAI = process.env.APP_APIAI_TOKEN;
 
-    static ENV = process.env.ENV;
-    static TOKEN = process.env.TOKEN;
-    static TOKEN_APIAI = process.env.TOKEN_APIAI;
-    static MONGODB = process.env.MONGODB;
+    static ENABLE = true;
+    static VERSION = 'v2.1.8';
+    static NAME = process.env.BOT_NAME;
+    static OWNER = process.env.BOT_OWNERS;
+    static PREFIX = process.env.BOT_PREFIX;
 }
