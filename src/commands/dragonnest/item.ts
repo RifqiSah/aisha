76edcbca7b9ca6ff547dab6d3e3c4c6bd3c4b8f2
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2020-06-28 23:57:46 +0700

    init: item command

---
+++ src/commands/dragonnest/item.ts
@@ -0,0 +1,9 @@
+const dv_server     = 'sea';
+const dv_endpoint   = `https://reference.dn.divinitor.com/api/server/${dv_server}/`;
+
+/*
+* https://reference.dn.divinitor.com/api/server/sea/tables/itemtable_reboot/268488032
+* https://reference.dn.divinitor.com/api/server/sea/uistring/1000160471
+* https://reference.dn.divinitor.com/api/server/sea/uistring/1000072229
+* https://reference.dn.divinitor.com/api/server/sea/uistring/1000161578
+*/
