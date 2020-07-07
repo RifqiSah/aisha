bc99b8f3e23d38aab84246957af7f9236f642fb6
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2020-07-07 14:01:55 +0700

    chore: bot version

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,4 +1,5 @@
 import './env';
+import values from './values';
 
 export default class Config {
     static ENV = process.env.APP_ENV;
@@ -6,7 +7,7 @@ export default class Config {
     static TOKEN = process.env.APP_DISCORD_TOKEN;
     static TOKEN_APIAI = process.env.APP_APIAI_TOKEN;
 
-    static BOT_VERSION = 'v2.1.24';
+    static BOT_VERSION = values.bot_version;
     static BOT_NAME = process.env.BOT_NAME;
     static BOT_OWNER = process.env.BOT_OWNERS;
     static BOT_PREFIX = process.env.BOT_PREFIX;
--- src/lib/values.ts
+++ src/lib/values.ts
@@ -1,4 +1,6 @@
 export default class Values {
+    static bot_version      = 'v2.1.31';
+
     static aisha_api        = 'https://alriftech.com/api/v2/bot/aisha';
     static divinitor_api    = 'https://reference.dn.divinitor.com/api/server/sea';
 }
