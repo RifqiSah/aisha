4629f1997214daad6b0545d2e4dd2a347cc3aa4d
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2020-07-09 11:39:39 +0700

    version bump!

--- src/lib/values.ts
+++ src/lib/values.ts
@@ -1,5 +1,5 @@
 export default class Values {
-    static bot_version      = 'v2.1.31';
+    static bot_version      = 'v2.2.00';
 
     static aisha_api        = 'https://alriftech.com/api/v2/bot/aisha';
     static divinitor_api    = 'https://reference.dn.divinitor.com/api/server/sea';
