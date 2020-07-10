d0fd00634d439b9751e7127e29c44941cf701f4b
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2020-07-10 15:51:52 +0700

    add: cat command

--- src/lib/values.ts
+++ src/lib/values.ts
@@ -3,4 +3,7 @@ export default class Values {
 
     static aisha_api        = 'https://alriftech.com/api/v2/bot/aisha';
     static divinitor_api    = 'https://reference.dn.divinitor.com/api/server/sea';
+
+    static cat_apikey       = '17151c18-d9a3-4ada-9a1f-e7e5747cb8c2';
+    static cat_api          = 'https://api.thecatapi.com/v1/images/search';
 }
