c578e4843b35b196fcc6f37afec15aa1eae304a7
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2020-07-23 07:03:23 +0700

    refactor: leave command is support array of guild

--- src/commands/subcmd/bot/bot/leave.ts
+++ src/commands/subcmd/bot/bot/leave.ts
@@ -1,19 +1,29 @@
 module.exports = {
     name: 'leave',
     func: async (client: any, message: any, args: any) => {
-        const guildId: string = args[1];
+        args.shift();
 
-        if (!guildId) {
-            return message.channel.send('Mohon masukkan Guild ID!').then((msg: any) => msg.delete({ timeout: 5000 })).catch((err: any) => {
+        const guildIds: string[] = args;
+        let counts = 0;
+
+        if (!guildIds.length) {
+            return message.channel.send('Mohon masukkan minimal 1 Guild ID!').then((msg: any) => msg.delete({ timeout: 5000 })).catch((err: any) => {
                 client.logger.error(err);
             });
         }
 
-        client.bot.guilds.cache.get(guildId).leave().catch((err: any) => {
-            return client.logger.error(err);
+        guildIds.forEach((id: string) => {
+            const guildObj = client.bot.guilds.cache.get(id);
+            if (guildObj) {
+                guildObj.leave().catch((err: any) => {
+                    return client.logger.error(err);
+                });
+
+                counts++;
+            }
         });
 
-        return message.channel.send(`Aisha telah keluar dari <${guildId}>!`).then((msg: any) => msg.delete({ timeout: 5000 })).catch((err: any) => {
+        return message.channel.send(`Aisha telah keluar dari ${counts} - ${guildIds.length} guild!`).then((msg: any) => msg.delete({ timeout: 5000 })).catch((err: any) => {
             client.logger.error(err);
         });
     }
