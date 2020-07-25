3b2bbe17d416bd7dd09e3cfe391fbc25b31b6325
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2020-07-25 19:22:02 +0700

    init: test function for point add

--- src/commands/bot/test.ts
+++ src/commands/bot/test.ts
@@ -1,6 +1,8 @@
 import func from '../../lib/function';
 import values from '../../lib/values';
 
+const psvc = require('./../../database/services/point.service');
+
 module.exports = {
     name: 'test',
     desc: 'Menguji coba fitur baru pada Aisha.',
@@ -12,6 +14,17 @@ module.exports = {
     usage: '',
     cooldown: 0,
     func: async (client: any, message: any, args: any) => {
-        message.channel.send('Dalam perbaikan! Tidak ada fitur baru!');
+        // message.channel.send('Dalam perbaikan! Tidak ada fitur baru!');
+
+        // psvc.addPoint('1234567890', 100);
+        // psvc.addPoint('1234567890', 200);
+        // psvc.addPoint('1234567890', 100);
+        // psvc.addPoint('1234567890', 400);
+
+        // psvc.addPoint('1234567890', -50);
+        // psvc.addPoint('1234567890', -100);
+
+        const sum = await psvc.total('1234567890');
+        console.log(sum);
     },
 };
