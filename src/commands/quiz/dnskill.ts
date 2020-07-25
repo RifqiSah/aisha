cbbe6d4a79850aa5196f2dec3c00810080702c49
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2020-07-25 19:23:56 +0700

    init: quiz

---
+++ src/commands/quiz/dnskill.ts
@@ -0,0 +1,14 @@
+module.exports = {
+    name: 'dnskill',
+    desc: 'Quiz menebak skill pada game Dragon Nest.',
+    enable: true,
+    regex: false,
+    help: true,
+    role: [],
+    aliases: [],
+    usage: '',
+    cooldown: 0,
+    func: (client: any, message: any, args: any) => {
+        return;
+    },
+};
