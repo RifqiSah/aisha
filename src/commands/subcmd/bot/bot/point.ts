a368621322f1d02e7588f849622fbbc4e673c3b8
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2020-07-26 07:00:51 +0700

    add: point command for manual add and deletion

---
+++ src/commands/subcmd/bot/bot/point.ts
@@ -0,0 +1,27 @@
+module.exports = {
+    name: 'point',
+    func: async (client: any, message: any, args: any) => {
+        const user = message.mentions.users.first();
+        
+        if (!user && !args[2]) {
+            return message.channel.send('Mohon mention user dan masukkan nominal point!').then((msg: any) => msg.delete({ timeout: 5000 })).catch((err: any) => {
+                client.logger.error(err);
+            });
+        }
+
+        if (args[2] == 'delete') {
+            client.pointsvc.delete(user.id);
+
+            return message.channel.send(`Berhasil menghapus semua point milik ${user.tag}!`).then((msg: any) => msg.delete({ timeout: 5000 })).catch((err: any) => {
+                client.logger.error(err);
+            });
+        }
+
+        const point = parseInt(args[2]);
+        client.pointsvc.addPoint(user.id, point);
+        
+        return message.channel.send(`Berhasil menambahkan point sebesar \`${point}\` kepada \`${user.tag}\`!`).then((msg: any) => msg.delete({ timeout: 5000 })).catch((err: any) => {
+            client.logger.error(err);
+        });
+    }
+};
