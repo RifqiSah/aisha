1d434dba99404ed7c188c924999c56853c5fcadb
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2020-07-26 08:06:46 +0700

    new feature, tada!

--- src/lib/values.ts
+++ src/lib/values.ts
@@ -1,5 +1,5 @@
 export default class Values {
-    static bot_version      = 'v2.4.19';
+    static bot_version      = 'v2.5.00';
 
     static aisha_api        = 'https://alriftech.com/api/v2/bot/aisha';
     static divinitor_api    = 'https://reference.dn.divinitor.com/api/server/sea';
