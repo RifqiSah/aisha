bd700fe51b7caef3a47124d2ebe051899ddf8143
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2021-04-12 14:00:19 +0700

    feat: add dragonnest guide command

---
+++ src/commands/dragonnest/guide.ts
@@ -0,0 +1,34 @@
+import func from '../../lib/function';
+const list = func.formatData('dnguide');
+
+module.exports = {
+    name: 'guide',
+    desc: `Melihat guide dari Dragon Nest. Guide yang tersedia yaitu:\n\n\`\`\`${list}\`\`\``,
+    enable: true,
+    regex: false,
+    help: true,
+    role: ['489292018628165633'],
+    aliases: [],
+    usage: '[jenis guide]',
+    cooldown: 0,
+    func: (client: any, message: any, args: any) => {
+        const guide = (args.length ? args.join(' ').toLowerCase() : 'null');
+        const msg = [];
+
+        const data = func.getExternalData('dragonnest.dnguide', guide);
+        if (!data) {
+            msg.push(`Guide untuk \`${guide}\` tidak ditemukan!`);
+
+            const recom = func.commandRecom('dnguide', guide);
+            if (recom) msg.push(`\nMungkin yang Anda maksud: \`${recom}\`?`);
+        } else {
+            msg.push(`__**Guide untuk ${data.name}**__\n`);
+            data.data.map((id: string) => {
+                msg.push(id);
+            });
+        }
+
+        msg.push(`\nGunakan \`${client.config.BOT_PREFIX}help guide\` untuk melihat daftar guide yang tersedia.\n`);
+        message.channel.send(msg, { split: true });
+    },
+};
