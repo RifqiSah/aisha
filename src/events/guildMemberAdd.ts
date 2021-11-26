9f9c7ccb63df66219f9ad28782ec8e05bbd8b8ad
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2021-11-26 13:10:14 +0700

    refactor: remove moderation command, using separate bot instead

--- src/events/guildMemberAdd.ts
+++ src/events/guildMemberAdd.ts
@@ -8,17 +8,4 @@ module.exports = (client: any, member: any) => {
 
     member.user.send('Terima kasih telah bergabung kedalam Discord milik Informate Squad 😃\nSilahkan membaca channel ``#peraturan`` terlebih dahulu sebelum memulai aktifitas didalam server Discord milik Informate Squad.\n\nTerima kasih 😃');
     channel.send(`Selamat datang di Informate Server, ${member}! Taati peraturan yang telah dibuat pada <#372926591849988096> demi kenyamanan kita bersama.\n\nTerima kasih 😃`); // #peraturan
-
-    // Untuk command verify
-    member.roles.add('669544469594374145');
-
-    // Logs
-    const data = [];
-
-    data.push('__**User Joined**__\n');
-    data.push(`${funct.getDate()}`);
-    data.push(`\`${member.user.id}\` ${member.user.tag}`);
-    data.push(`<${member.user.avatarURL()}>`);
-
-    member.guild.channels.cache.find((ch: any) => ch.id === '496220491988729856').send(data, { split: true }); // log-aisha
 };
--- src/events/guildMemberRemove.ts
+++ src/events/guildMemberRemove.ts
@@ -7,15 +7,4 @@ module.exports = (client: any, member: any) => {
     }
 
     channel.send(`Oh tidakk, ${member} telah keluar dari server kita 😔`);
-
-    // Logs
-    const data = [];
-
-    data.push('__**User Left**__\n');
-
-    data.push(`${funct.getDate()}`);
-    data.push(`\`${member.user.id}\` ${member.user.tag}`);
-    data.push(`<${member.user.avatarURL()}>`);
-
-    member.guild.channels.cache.find((ch: any) => ch.id === '496220491988729856').send(data, { split: true }); // log-aisha
 };
