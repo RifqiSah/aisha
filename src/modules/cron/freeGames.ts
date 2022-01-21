da9448ed966f4bb7e42a206c971465ca1df77d86
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2022-01-21 17:03:28 +0700

    feat: add free game cron module

--- src/modules/cron/freeGames.ts
+++ src/modules/cron/freeGames.ts
@@ -1,7 +1,39 @@
+import moment from 'moment';
 import cron from 'node-cron';
 
-const getData = () => {
-    console.log('Hahaha!');
+let _client: any = null;
+
+import Parser from 'rss-parser';
+const parser = new Parser();
+
+const getData = async () => {
+    _client.logger.debug('[CRON] FreeGames ticked!');
+    try {
+        const data: any[] = [];
+        let latestData = await _client.globalsvc.getGlobals('news.freegames');
+        latestData = moment(latestData.value);
+
+        const feed = await parser.parseURL('https://steamcommunity.com/groups/freegamesfinders/rss/');
+        for (let i = feed.items.length - 1; i >= 0; i--) {
+            const item = feed.items[i];
+            const itemDate = moment(item.pubDate);
+
+            if (itemDate.isAfter(latestData)) {
+                console.log(item.title);
+                console.log(itemDate);
+
+                if (i === 0) {
+                    await _client.globalsvc.updateGlobals('news.freegames', item.pubDate);
+                }
+            }
+        }
+    } catch (err: any) {
+        _client.logger.error('[CRON] An error occured!', err);
+    }
+};
+
+exports.init = (client: any) => {
+    _client = client;
 };
 
 export const handle = cron.schedule('* * * * *', () => {
--- src/modules/cron/index.ts
+++ src/modules/cron/index.ts
@@ -7,11 +7,12 @@ handle.start();
 module.exports = (client: any) => {
     client.logger.info('  [-] Initialize cron');
 
-    const crons = readdirSync(resolve(__dirname, './')).filter((f) => f.endsWith('.js') && !f.includes('index.js'));
+    const crons = readdirSync(resolve(__dirname, './')).filter((f) => f.endsWith('.js') && !f.includes('index.js') && !f.includes('example.js'));
     for (const file of crons) {
         const cron = require(resolve(__dirname, `./${file}`));
         const key = file.slice(0, -3);
 
+        cron.init(client);
         cron.handle.start();
 
         client.logger.info(`    + '${key}' added and started.`);
