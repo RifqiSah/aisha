011d1db7de76024a2bb6cf1df8dfd9bb20a0d6ab
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2022-02-28 09:15:13 +0700

    change: to hourly

--- src/modules/cron/src/freeGames.ts
+++ src/modules/cron/src/freeGames.ts
@@ -64,6 +64,6 @@ exports.init = (client: any) => {
     _client = client;
 };
 
-export const handle = cron.schedule('* * * * *', () => {
+export const handle = cron.schedule('0 * * * *', () => {
     getData();
 });
