bc0a9575109dda35c309c7088054eb5ab6dec551
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2022-04-20 09:56:39 +0700

    feat: change aisha api path

--- src/lib/values.ts
+++ src/lib/values.ts
@@ -4,7 +4,7 @@ import config from './config';
 export default class Values {
     static bot_version      = 'v2.6.0';
 
-    static aisha_api        = `${process.env.ENDPOINT}/api/v2/bot/aisha`;
+    static aisha_api        = `${process.env.ENDPOINT}/bot/aisha`;
     static divinitor_api    = 'https://reference.dn.divinitor.com/api/server/sea';
 
     static cat_apikey       = '17151c18-d9a3-4ada-9a1f-e7e5747cb8c2';
