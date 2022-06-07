e1b4124dbadc340c489ff12164a13771a537365f
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2022-06-07 14:23:28 +0700

    fix: crash if ws is closed

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,17 +1,21 @@
 module.exports = (client: any) => {
-    client.bot.user.setUsername(client.config.BOT_NAME);
-    // bot.user.setAvatar("");
+    try {
+        client.bot.user.setUsername(client.config.BOT_NAME);
+        // bot.user.setAvatar("");
 
-    client.logger.info(`[V] Bot has started, with ${client.bot.users.cache.size} users, in ${client.bot.channels.cache.size} channels of ${client.bot.guilds.cache.size} guilds.`);
+        client.logger.info(`[V] Bot has started, with ${client.bot.users.cache.size} users, in ${client.bot.channels.cache.size} channels of ${client.bot.guilds.cache.size} guilds.`);
 
-    const activitiesList = [
-        `${client.config.BOT_PREFIX}h for command.`,
-        "Informate's Bot.",
-        `Serving ${client.bot.users.cache.size} users in ${client.bot.guilds.cache.size} guilds.`,
-    ];
+        const activitiesList = [
+            `${client.config.BOT_PREFIX}h for command.`,
+            "Informate's Bot.",
+            `Serving ${client.bot.users.cache.size} users in ${client.bot.guilds.cache.size} guilds.`,
+        ];
 
-    setInterval(() => {
-        // bot.user.setStatus('dnd');
-        client.bot.user.setActivity(activitiesList[Math.floor(Math.random() * activitiesList.length)]/* , { type: "WATCHING" } */);
-    }, 10000);
+        setInterval(() => {
+            // bot.user.setStatus('dnd');
+            client.bot.user.setActivity(activitiesList[Math.floor(Math.random() * activitiesList.length)]/* , { type: "WATCHING" } */);
+        }, 10000);
+    } catch (err: any) {
+        client.logger.error(err);
+    }
 };
