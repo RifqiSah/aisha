8aed87a21ea8bdeb9753348f6487d06798972ea2
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2022-08-28 16:23:51 +0700

    feat: slash command for Dragon Nest

--- src/commands/dragonnest/dndrop.ts
+++ src/commands/dragonnest/dndrop.ts
@@ -1,10 +1,11 @@
-import { Message } from 'discord.js';
+import { Message, CommandInteraction, AutocompleteInteraction } from 'discord.js';
 
 import Command from '../../classes/command';
-import { getExternalData, commandRecom, formatImageInMessage, sendMessage, formatData } from '../../helpers/function';
+import { getExternalData, commandRecom, formatImageInMessage, sendMessage, formatData, formatDataAutocomplete, formatImageInInteraction, searchAutoComplete } from '../../helpers/function';
 import config from '../../lib/config';
 
 const list = formatData('dragonnest.dndrop');
+const listAuto = formatDataAutocomplete('dragonnest.dndrop');
 
 export default class DnDrop extends Command {
     constructor() {
@@ -12,6 +13,16 @@ export default class DnDrop extends Command {
             name: `Melihat info drop rate dari sebuah item pada Dragon Nest. Drop yang tersedia yaitu:\n\n\`\`\`${list}\`\`\``,
             command: 'dndrop',
             usage: '[nama item]',
+            registerSlashCommand: true,
+            hasAutocomplete: true,
+            slashCommandOptions: [
+                {
+                    name: 'keyword',
+                    description: 'Nama Item / Info / Apapun',
+                    type: 'STRING',
+                    autocomplete: true,
+                },
+            ],
         });
     }
 
@@ -32,5 +43,30 @@ export default class DnDrop extends Command {
 
         msg.push(`\nGunakan \`${config.BOT_PREFIX}help dndrop\` untuk melihat daftar drop yang tersedia.\n`);
         sendMessage(message, msg);
+    }
+
+    async interact(interaction: CommandInteraction): Promise<void> {
+        const msg: string[] = [];
+
+        try {
+            await interaction.deferReply();
+
+            const keyword = interaction.options.get('keyword')?.value;
+            const data = getExternalData('dragonnest.dndrop', `${keyword}`);
+
+            msg.push(`__**Drop rate untuk ${data.name}**__\n`);
+            await formatImageInInteraction(msg, interaction, data);
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    async autocomplete(interaction: AutocompleteInteraction): Promise<void> {
+        if (interaction.commandName !== this.command) return;
+
+        const keyword = interaction.options.get('keyword')?.value ?? '';
+        const search = await searchAutoComplete('dragonnest.dndrop', keyword as string);
+
+        await interaction.respond(search);
     }
 }
