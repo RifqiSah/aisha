5ed46200123ee72f74090ae941323ab81b799c73
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2023-12-29 09:13:51 +0700

    feat mc server status check after vm is on

--- src/commands/minecraft/mcnew.ts
+++ src/commands/minecraft/mcnew.ts
@@ -7,14 +7,46 @@ import axios from '../../lib/axios';
 import config from '../../lib/config';
 import { logger } from '../../lib/logger';
 
+/**
+ * Virtual Machine
+ */
+const vmInfo = async () => {
+    const result = await axios.get(`${config.IDCLOUDHOST_API}/user-resource/vm?uuid=${config.IDCLOUDHOST_UUID}`, { headers: { apikey: `${config.IDCLOUDHOST_APIKEY}` } });
+    return result?.data;
+};
+
 const vmStatus = async () => {
-    const result = await axios.get(`${config.IDCLOUDHOST_API}?uuid=${config.IDCLOUDHOST_UUID}`, { headers: { apikey: `${config.IDCLOUDHOST_APIKEY}` } });
-    return result?.data?.status === 'running';
+    const result = await vmInfo();
+    return result?.status === 'running';
 };
 
 const vmOperate = async (status: string) => {
-    const result = await axios.post(`${config.IDCLOUDHOST_API}/${status}`, { uuid: `${config.IDCLOUDHOST_UUID}` }, { headers: { apikey: `${config.IDCLOUDHOST_APIKEY}` } });
+    const result = await axios.post(`${config.IDCLOUDHOST_API}/user-resource/vm/${status}`, { uuid: `${config.IDCLOUDHOST_UUID}` }, { headers: { apikey: `${config.IDCLOUDHOST_APIKEY}` } });
     return result?.data;
+};
+
+/**
+ * Assigned IP
+ */
+const vmIp = async () => {
+    const result = await axios.get(`${config.IDCLOUDHOST_API}/network/ip_addresses?vm_uuid=${config.IDCLOUDHOST_UUID}`, { headers: { apikey: `${config.IDCLOUDHOST_APIKEY}` } });
+    return result?.data;
+};
+
+/**
+ * Minecraft Server
+ */
+const mcServerStatus = async () => {
+    const ip = await vmIp();
+
+    if (!ip?.length) {
+        throw new Error('Unable to get VM ip !');
+    }
+
+    const resultIp = ip[0].address;
+    const serverResult = await axios.get(`https://api.mcstatus.io/v2/status/java/${resultIp}`, { headers: { 'Accept-Encoding': '*' } });
+
+    return serverResult?.data?.online === true;
 };
 
 export default class McNew extends Command {
@@ -45,26 +77,39 @@ export default class McNew extends Command {
             const timeout = 1000;
 
             let refreshId: any = null;
+            let refreshIdMc: any = null;
+
             let status = false;
 
             const cmd = interaction.options.get('cmd')?.value;
             switch (cmd) {
                 case 'start':
                     status = await vmStatus();
-                    if (status) {
-                        await interaction.editReply({ content: 'VM sudah menyala!' });
-                        break;
-                    }
+                    // if (status) {
+                    //     await interaction.editReply({ content: 'VM sudah menyala!' });
+                    //     break;
+                    // }
 
-                    await vmOperate('start');
+                    // await vmOperate('start');
 
                     refreshId = setInterval(async () => {
                         status = await vmStatus();
                         if (status) {
-                            await interaction.editReply({ content: 'VM telah dinyalakan! Mohon menunggu sampai server Minecraft menyala 😃' });
+                            await interaction.editReply({ content: '1/2 - VM telah dinyalakan! Mohon menunggu sampai server Minecraft menyala 😃' });
                             clearInterval(refreshId);
                         }
                     }, timeout);
+
+                    // vm on?
+                    if (status) {
+                        refreshIdMc = setInterval(async () => {
+                            status = await mcServerStatus();
+                            if (status) {
+                                await interaction.editReply({ content: '2/2 - Server telah dinyalakan! Selamat bermain 😃' });
+                                clearInterval(refreshIdMc);
+                            }
+                        }, timeout);
+                    }
 
                     break;
 
